5f528eb17a322ce2b37581d51d24f5360d0641a0
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-03-18 15:37:49 +0500

    Fixed Directory name

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/tests/setup.js',
+    setupFiles: './src/__tests__/setup.js',
   },
 });
