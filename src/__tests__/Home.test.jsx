c8625b367bd4a9ccfc2dc269d900b32bb4056469
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-03-18 16:51:15 +0500

    Fixed path

--- src/__tests__/Home.test.jsx
+++ src/__tests__/Home.test.jsx
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
-import store from '../Redux/store';
+import store from '../redux/store';
 
 describe('App', () => {
   it('renders Home', () => {
--- src/__tests__/Navigation.test.jsx
+++ src/__tests__/Navigation.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
-import store from '../Redux/store';
+import store from '../redux/store';
 import Navigation from '../components/Navigation';
 
 describe('App', () => {
